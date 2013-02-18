e42ab29ce31420788bcff89ce10cb46209996441
Mitch Foley <mitch@thefoley.net>
2013-02-17 19:55:09 -0500

    I forgot what this was. But I'm sure it's important.

--- html/mojhosto.js
+++ html/mojhosto.js
@@ -1,13 +1,30 @@
 $(function() {
+  var card = JSON.stringify({
+      arr: ['hello world 1\n', 'hello world 2\n'] // make sure they end in \n
+  });
   function pheldy(e) {
     var injectedObject = window.injectedObject || window.$injectedObject;
     if (injectedObject && injectedObject.printCard) {
-      window.console.log(injectedObject.printCard());
+	window.console.log(injectedObject.printCard(card));
     } else {
       window.console.log('miss');
     }
   }
+  function jho(instant) {
+    var text = instant ? 'instant' : 'sorcery';
+    $('#jho>div').each(function(i, elem) {
+      $(elem).text(text + ' ' + Math.random());
+    });
+  }
+  function instant(e) {
+    jho(true);
+  }
+  function sorcery(e) {
+    jho(false);
+  }
   $('#pheldy').click(pheldy);
+  $('#instant').click(instant);
+  $('#sorcery').click(sorcery);
 });
 function onUpdateReady() {
     window.console.log('update ready');
