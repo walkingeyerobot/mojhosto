67d5679c884b996895bb2b182b763d33ba02d603
Mitch Foley <mitch@thefoley.net>
2013-02-27 00:20:48 -0500

    initial wiring of creature and equipment buttons

--- html/mojhosto.js
+++ html/mojhosto.js
@@ -13,6 +13,29 @@ $(function() {
   function printPheldy(e) {
     window.console.log(injectedObject.printPheldy());
   }
+  function creature(e) {
+    console.log('creature ' + $('#creature-cmc').val());
+  }
+  function creatureCmc(e) {
+    var v = parseInt($(this).val(), 10);
+    if (v === 0) {
+      v = '-';
+    } else if (v > 8) {
+      v = 8;
+    }
+    $('#equipment-cmc').val(v+'');
+    equipmentCmc.call($('#equipment-cmc'), e);
+  }
+  function equipment(e) {
+    console.log('equipment <' + $('#equipment-cmc').val());
+  }
+  function equipmentCmc(e) {
+    if ($(this).val() === '-') {
+      $('#equipment').attr('disabled', 'disabled');
+    } else {
+      $('#equipment').removeAttr('disabled');
+    }
+  }
   function jho(instant) {
     var text = instant ? 'instant' : 'sorcery';
     $('#jho>div').each(function(i, elem) {
@@ -27,6 +50,10 @@ $(function() {
   }
   $('#pheldy').click(printPheldy);
   $('#card').click(printCard);
+  $('#creature').click(creature);
+  $('#creature-cmc').change(creatureCmc);
+  $('#equipment').click(equipment);
+  $('#equipment-cmc').change(equipmentCmc);
   $('#instant').click(instant);
   $('#sorcery').click(sorcery);
 });
